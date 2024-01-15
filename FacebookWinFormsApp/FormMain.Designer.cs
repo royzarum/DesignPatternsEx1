5fde50207ce83ad40c3c4a740ce8a13cc663c7c0
royg24 <roy@goldhar.net>
2024-01-15 14:01:16 +0200

    build the basic main form and create the template form

--- FacebookWinFormsApp/FormMain.Designer.cs
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.buttonLogout = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BirthdayLabel = new System.Windows.Forms.Label();
             this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
-            this.textBoxAppID = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.GenderLabel = new System.Windows.Forms.Label();
+            this.UserGenderLabel = new System.Windows.Forms.Label();
+            this.UserBirthdayLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogin
@@ -64,17 +69,6 @@
             this.buttonLogout.UseVisualStyleBackColor = true;
             this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(314, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(559, 36);
-            this.label1.TabIndex = 53;
-            this.label1.Text = "This is the AppID of \"Design Patterns App 2.4\". The grader will use it to test yo" +
-    "ur app.\r\nType here your own AppID to test it:";
-            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
@@ -88,18 +82,29 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.UserBirthdayLabel);
+            this.tabPage1.Controls.Add(this.UserGenderLabel);
+            this.tabPage1.Controls.Add(this.GenderLabel);
+            this.tabPage1.Controls.Add(this.BirthdayLabel);
             this.tabPage1.Controls.Add(this.pictureBoxProfile);
-            this.tabPage1.Controls.Add(this.textBoxAppID);
-            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.buttonLogout);
             this.tabPage1.Controls.Add(this.buttonLogin);
-            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Location = new System.Drawing.Point(4, 35);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1235, 666);
+            this.tabPage1.Size = new System.Drawing.Size(1235, 658);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BirthdayLabel
+            // 
+            this.BirthdayLabel.AutoSize = true;
+            this.BirthdayLabel.Location = new System.Drawing.Point(632, 23);
+            this.BirthdayLabel.Name = "BirthdayLabel";
+            this.BirthdayLabel.Size = new System.Drawing.Size(98, 26);
+            this.BirthdayLabel.TabIndex = 57;
+            this.BirthdayLabel.Text = "Birthday:";
             // 
             // pictureBoxProfile
             // 
@@ -110,27 +115,48 @@
             this.pictureBoxProfile.TabIndex = 55;
             this.pictureBoxProfile.TabStop = false;
             // 
-            // textBoxAppID
-            // 
-            this.textBoxAppID.Location = new System.Drawing.Point(317, 61);
-            this.textBoxAppID.Name = "textBoxAppID";
-            this.textBoxAppID.Size = new System.Drawing.Size(237, 24);
-            this.textBoxAppID.TabIndex = 54;
-            this.textBoxAppID.Text = "1551435755672177";
-            // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Location = new System.Drawing.Point(4, 35);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1235, 666);
+            this.tabPage2.Size = new System.Drawing.Size(1235, 658);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // GenderLabel
+            // 
+            this.GenderLabel.AutoSize = true;
+            this.GenderLabel.Location = new System.Drawing.Point(971, 28);
+            this.GenderLabel.Name = "GenderLabel";
+            this.GenderLabel.Size = new System.Drawing.Size(90, 26);
+            this.GenderLabel.TabIndex = 58;
+            this.GenderLabel.Text = "Gender:";
+            // 
+            // UserGenderLabel
+            // 
+            this.UserGenderLabel.AutoSize = true;
+            this.UserGenderLabel.Location = new System.Drawing.Point(1067, 28);
+            this.UserGenderLabel.Name = "UserGenderLabel";
+            this.UserGenderLabel.Size = new System.Drawing.Size(140, 26);
+            this.UserGenderLabel.TabIndex = 59;
+            this.UserGenderLabel.Text = "GenderValue";
+            this.UserGenderLabel.Visible = false;
+            // 
+            // UserBirthdayLabel
+            // 
+            this.UserBirthdayLabel.AutoSize = true;
+            this.UserBirthdayLabel.Location = new System.Drawing.Point(735, 28);
+            this.UserBirthdayLabel.Name = "UserBirthdayLabel";
+            this.UserBirthdayLabel.Size = new System.Drawing.Size(96, 26);
+            this.UserBirthdayLabel.TabIndex = 60;
+            this.UserBirthdayLabel.Text = "1/1/2000";
+            this.UserBirthdayLabel.Visible = false;
+            // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1243, 697);
             this.Controls.Add(this.tabControl1);
@@ -143,6 +169,7 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,12 +178,15 @@
 
 		private System.Windows.Forms.Button buttonLogin;
 		private System.Windows.Forms.Button buttonLogout;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TabControl tabControl1;
 		private System.Windows.Forms.TabPage tabPage1;
 		private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TextBox textBoxAppID;
         private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.Label BirthdayLabel;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label UserBirthdayLabel;
+        private System.Windows.Forms.Label UserGenderLabel;
+        private System.Windows.Forms.Label GenderLabel;
     }
 }
 
