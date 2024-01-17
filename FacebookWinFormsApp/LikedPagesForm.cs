f0a524b566f4e96804fb7b4002970a048b4b7f37
royg24 <roy@goldhar.net>
2024-01-17 19:39:55 +0200

    main form upgraded, remember me added but not working because of premission problems

---
+++ FacebookWinFormsApp/LikedPagesForm.cs
@@ -0,0 +1,22 @@
+﻿using FacebookWrapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures
+{
+    public partial class LikedPagesForm : Form
+    {
+        public LikedPagesForm()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
